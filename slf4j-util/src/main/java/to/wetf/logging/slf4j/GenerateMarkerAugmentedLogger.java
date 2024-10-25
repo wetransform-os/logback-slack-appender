3eb94e983c7aa295b97096bd2d39ce74418c1b4b
Simon Templer <simon@wetransform.to>
2024-10-25 17:17:22 +0200

    feat: add augmented logger that can generate a marker on event time

---
+++ slf4j-util/src/main/java/to/wetf/logging/slf4j/GenerateMarkerAugmentedLogger.java
@@ -0,0 +1,32 @@
+package to.wetf.logging.slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.Marker;
+import java.util.function.Supplier;
+
+/**
+ * Logger that augments log events with marker information.
+ * The marker is generated on each log event.
+ *
+ * @author Simon Templer
+ */
+public class GenerateMarkerAugmentedLogger extends AbstractMarkerAugmentedLogger {
+
+  private final Supplier<Marker> marker;
+
+  public GenerateMarkerAugmentedLogger(Logger logger, String fqcn, Supplier<Marker> marker) {
+    super(logger, fqcn);
+    this.marker = marker;
+  }
+
+  public GenerateMarkerAugmentedLogger(Logger logger, Supplier<Marker> marker) {
+    super(logger);
+    this.marker = marker;
+  }
+
+  @Override
+  protected Marker augmentMarker(Marker marker) {
+    return Markers.combineMarkers(marker, this.marker.get());
+  }
+
+}
